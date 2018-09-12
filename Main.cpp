1b164cad51267ed59eccfd40ed96c8a3e8d74f09
Dovahstrun <43002140+Dovahstrun@users.noreply.github.com>
2018-09-12 16:09:37 +0100

    Created some variables for the timer

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,69 @@
-#include <SFML/Graphics.hpp>
-int main()
-{
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+//Included libraries
+#include <sfml/Graphics.hpp> //Includes graphics
+#include <SFML/Audio.hpp> //Includes audio
+#include <string> //Include strings
+//End libraries
 
-	while (window.isOpen())
+int main()
+{	
+	///-------------------------------------------------
+	///SETUP
+	///-------------------------------------------------
+
+	//render window creation
+	sf::RenderWindow gameWindow;
+	gameWindow.create(sf::VideoMode::getDesktopMode(), "Button Masher", sf::Style::Titlebar | sf::Style::Close);
+
+	///-------------------------------------------------
+	///END SETUP
+	///-------------------------------------------------
+
+	///-------------------------------------------------
+	///Game Loop
+	///-------------------------------------------------
+
+	while (gameWindow.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		///---------------------------------------------
+		///INPUT
+		///---------------------------------------------
+
+		sf::Event gameEvent;
+		while (gameWindow.pollEvent(gameEvent))
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-		window.clear();
-		window.draw(shape);
-		window.display();
+			//Process Events
+
+			//Check if the event is the closed event
+			if (gameEvent.type == sf::Event::Closed)
+			{
+				//Close the game window
+				gameWindow.close();
+			}//End Closed event
+		}//End input polling while loop
+
+		///---------------------------------------------
+		///END INPUT
+		///---------------------------------------------
+
+		///---------------------------------------------
+		///UPDATE
+		///---------------------------------------------
+
+
+		///---------------------------------------------
+		///END UPDATE
+		///---------------------------------------------
+
+		///---------------------------------------------
+		///DRAW
+		///---------------------------------------------
+		gameWindow.clear();
+		gameWindow.display();
 	}
+
+	///-------------------------------------------------
+	///END GAME LOOP
+	///-------------------------------------------------
 
 	return 0; //Letus do the time warp again
 }
